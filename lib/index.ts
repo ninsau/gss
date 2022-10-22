d6c80d2586ed6703850263da6126b43411b7cd72
ninsau <gideon@hubtel.com>
2022-10-22 21:03:55 +0000

    initial commit

--- lib/index.ts
+++ lib/index.ts
@@ -1,12 +1,12 @@
-export const BRAND_NAME: string = "PottersTouch LLC - Healthcare & Transportation Services";
+export const BRAND_NAME: string = "GSS Coupons";
 
-export const BRAND_TAGLINE: string = "Your Safty and Health is Our Priority";
+export const BRAND_TAGLINE: string = "Save money with coupons";
 
 export const BRAND_DESCRIPTION: string = " ";
 
-export const BRAND_URL: string = "https://www.potterstouch.org";
+export const BRAND_URL: string = "";
 
-export const BRAND_IMAGE: string = "https://res.cloudinary.com/deyudesls/image/upload/v1666366866/potterstouchlogo.webp";
+export const BRAND_IMAGE: string = "";
 
 export const BRAND_FAVICON: string = "/android-chrome-192x192.png";
 
