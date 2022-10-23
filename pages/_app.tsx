0c00bd47e4dacb02d599f2b600fa15aa9d430c15
ninsau <gideon@hubtel.com>
2022-10-23 01:52:28 +0000

    [Fixed] missing app sync

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,17 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { Toaster } from "react-hot-toast";
+import { Amplify } from "aws-amplify";
+import config from "../src/aws-exports.js";
+
+
+Amplify.configure({
+  ...config,
+  ssr: true,
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
+  
   return (
     <>
       <Toaster position="top-center" reverseOrder={false} />
